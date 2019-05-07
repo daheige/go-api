b53753badba42c0f1f7ea313de82a3ff201e0cc0
daheige <zhuwei313@hotmail.com>
2019-05-07 22:24:11 +0800

    logger格式

--- app/extensions/Logger/log.go
+++ app/extensions/Logger/log.go
@@ -11,6 +11,34 @@ import (
 	"github.com/daheige/thinkgo/common"
 )
 
+/**
+ * {
+    "level": 200,
+    "level_name": "info",
+    "local_time": "2019-05-07 22:21:56.941",
+    "msg": "exec end",
+    "line_no": 41,
+    "file_path": "/web/go/go-api/app/extensions/Logger/log.go",
+    "context":
+    {
+        "current_file": "/web/go/go-api/app/middleware/LogWare.go",
+        "current_line": 44,
+        "host": "[::1]:56752",
+        "ip": "::1",
+        "local_time": "2019-05-07 22:21:56.941",
+        "log_id": "a85367902fd194f35338ca640dc9509c",
+        "method": "GET",
+        "options":
+        {
+            "exec_time": 0.000480627
+        },
+        "plat": "web",
+        "request_uri": "/",
+        "tag": "_",
+        "ua": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+}
+*/
 const logTmFmtWithMS = "2006-01-02 15:04:05.999"
 
 func writeLog(ctx *gin.Context, levelName string, message interface{}, context map[string]interface{}) {
@@ -26,8 +54,9 @@ func writeLog(ctx *gin.Context, levelName string, message interface{}, context m
 		"request_uri":  ctx.Request.RequestURI,
 		"log_id":       ctx.GetString("log_id"),
 		"local_time":   time.Now().Format(logTmFmtWithMS),
-		"detail":       context,
+		"options":      context,
 		"host":         ctx.Request.RemoteAddr,
+		"ip":           ctx.ClientIP(),
 		"ua":           ua,
 		"plat":         helper.GetDeviceByUa(ua), //当前设备匹配
 		"method":       ctx.Request.Method,
